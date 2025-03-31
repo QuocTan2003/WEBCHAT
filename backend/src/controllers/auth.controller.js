11250b5d817765d284f069e23035863ba6775c9f
Quoc Tan <quoctantran1203@gmail.com>
2025-04-01 00:14:05 +0700

    Frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
+    console.log("JWT_SECRET:", process.env.JWT_SECRET);
   const { fullName, email, password } = req.body;
   try {
     if (!fullName || !email || !password) {
